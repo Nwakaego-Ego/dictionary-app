082a7b91e012fac29b9b5ba3c613238281998e8b
Nwakaego-Ego <glorynwaekpe@gmail.com>
2022-01-04 13:23:12 +0100

    Dictionary app set up

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './shecodes-logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>hello</h1>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
