161ee3fb2ad48cd5d9fb2ee8217843ecbdbf7d5f
Nwakaego-Ego <glorynwaekpe@gmail.com>
2022-01-05 19:05:16 +0100

    installed axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,20 @@
+import axios from "axios";
 import React, { useState } from "react";
 import "./Dictionary.css"
 
 export default function Dictionary () {
     const [search, setSearch] = useState("");
 
+    function handleClick(response) {
+        console.log(response.data[0])
+    }
+
     function submitSearch(event) {
         event.preventDefault();
-        alert(`searching for the definition of ${search}`)
+        // alert(`searching for the definition of ${search}`)
+
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${search}`
+        axios.get(apiUrl).then(handleClick)
       
     }
     
