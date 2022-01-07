5d6eaec705a050612b06f8a88beb6bbab04dcc85
Nwakaego-Ego <glorynwaekpe@gmail.com>
2022-01-07 12:21:04 +0100

    deleted stuff

--- src/Result.js
+++ src/Result.js
@@ -10,9 +10,7 @@ export default function Result(props) {
                         <div>
                             {definition.definitions[0]}
                         </div>
-                        <div>
-                        {definition.definitions[0]}
-                    </div>
+                      
                     )
                 })}
         </div>
