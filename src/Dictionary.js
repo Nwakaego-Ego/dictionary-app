a0f0c18942fe82253684d775a17f5a3702fd60d7
Nwakaego-Ego <glorynwaekpe@gmail.com>
2022-01-07 12:08:25 +0100

    Result.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,31 +1,35 @@
 import axios from "axios";
 import React, { useState } from "react";
+import Result from "./Result"
 import "./Dictionary.css"
 
 export default function Dictionary () {
     const [search, setSearch] = useState("");
+    const [result, setResult] = useState("")
 
     function handleClick(response) {
         console.log(response.data[0])
+        setResult(response.data[0])
     }
 
     function submitSearch(event) {
         event.preventDefault();
         // alert(`searching for the definition of ${search}`)
-
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${search}`
-        axios.get(apiUrl).then(handleClick)
-      
+        axios.get(apiUrl).then(handleClick) 
     }
     
     function searchResult(event) {
-        
-        setSearch(event.target.value)
+         setSearch(event.target.value)
     }
 
     return (
-        <form onSubmit={submitSearch}>
-            <input type="search" placeholder="search...." onChange={searchResult}/>
-        </form>
+        <div className="Dictionary">
+            <form onSubmit={submitSearch}>
+                 <input type="search" placeholder="search...." onChange={searchResult}/>
+            </form>
+            <Result result={result}/>
+        </div>
+        
     )
 }
---
+++ src/Result.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Result(props) {
+    console.log(props.result)
+    return (
+        <div>
+            <h2>{props.result.word}</h2>
+                {props.result.meanings.map(function (definition, index){
+                    return (
+                        <div>
+                            {definition.definitions[0]}
+                        </div>
+                    )
+                })}
+        </div>
+    )
+}
