74f79969d1a15136070ff659363fadefad35631c
Nwakaego-Ego <glorynwaekpe@gmail.com>
2022-01-11 17:32:42 +0100

    created Synonyms component and added a slice array

--- src/Definition.js
+++ src/Definition.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Definition (props) {
-    console.log(props.definition)
+    console.log(props)
 return (
     <div className="Definition">
         <p className="partofspeech">{props.definition.partOfSpeech}</p>
@@ -9,7 +10,9 @@ return (
             return (
                 <div key={index}>
                      <p>{meaning.definition}</p>
-                      <p><em>{meaning.example}</em></p>
+                      <p><strong>Example: </strong><em>{meaning.example}</em></p>
+                      <Synonyms synonyms={meaning.synonyms} />
+                      
                 </div>
               
             )
