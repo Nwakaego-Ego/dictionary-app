c958e73281e193aa78318eee526c1f731e621f3b
Nwakaego-Ego <glorynwaekpe@gmail.com>
2022-01-12 19:23:57 +0100

    created a Phonetics component.js

--- src/Definition.js
+++ src/Definition.js
@@ -2,11 +2,17 @@ import React from "react";
 import Synonyms from "./Synonyms";
 
 export default function Definition (props) {
+    
     console.log(props)
 return (
     <div className="Definition">
-        <p className="partofspeech">{props.definition.partOfSpeech}</p>
-        {props.definition.definitions.map(function (meaning, index) {
+        <p className="partofspeech">{props.definition.partOfSpeech}</p> 
+          {props.definition.definitions[0].definition}
+          <p><strong>Example: </strong><em>{props.definition.definitions[0].example}</em></p>
+          <Synonyms synonyms={props.definition.definitions[0].synonyms} />
+
+
+        {/* {props.definition.definitions.map(function (meaning, index) {
             return (
                 <div key={index}>
                      <p>{meaning.definition}</p>
@@ -16,7 +22,7 @@ return (
                 </div>
               
             )
-        })}
+        })} */}
        
     </div>
     
--- src/Result.js
+++ src/Result.js
@@ -1,12 +1,22 @@
 import React from "react";
 import Definition from "./Definition"
+import Phonetics from "./Phonetics"
 
 export default function Result(props) {
     console.log(props.result)
+    console.log(props)
 if ( props.result) {
     return (
         <div>
           <h3 className="text-capitalize">{props.result.word}</h3>
+            {props.result.phonetics.map(function (phonetic, index ) {
+                return (
+                    <div key={index}>
+                        <Phonetics phonetic={phonetic} />
+                    </div>
+                )
+            })}
+             
                 {props.result.meanings.map(function (definition, index){
                     return (
                         <div key={index}>
